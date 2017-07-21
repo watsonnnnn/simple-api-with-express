b5ca9adb173f8e381005b2dd1005df207a525ded
nicelj <nicelj92@gmail.com>
2017-07-21 18:56:07 +0800

    add

--- src/controller/articles/article.js
+++ src/controller/articles/article.js
@@ -1,12 +1,13 @@
 /**
  * Created by Administrator on 2017/7/18.
  */
+import articleModel from '../../models/article';
 class Article{
     getArticles(){
 
     }
     writeArticle(){
-
+        articleModel.create();
     }
     readArticle(){
 
